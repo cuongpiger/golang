51f5197c6f2bd51ecc66845b9c5ba5c3117f9208
Manh Cuong <cuongpigerr@gmail.com>
2022-06-25 23:26:27 +0700

    This is Manh Cuong's commit 🌻.

--- building-distributed-applications-in-gin/chap_02/recipes-api/main.go
+++ building-distributed-applications-in-gin/chap_02/recipes-api/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
 	"time"
 
@@ -20,6 +22,8 @@ type Recipe struct {
 var recipes []Recipe
 func init() {
 	recipes = make([]Recipe, 0)
+	file, _ := ioutil.ReadFile("recipes.json")  // read the json file into file variable
+	json.Unmarshal([]byte(file), &recipes)  // save file to recipes slice
 }
 
 func NewRecipeHandler(c *gin.Context) {
@@ -32,13 +36,18 @@ func NewRecipeHandler(c *gin.Context) {
 	recipe.ID = xid.New().String()  // generate a new id
 	recipe.PublishedAt = time.Now() // set the publishedAt to now
 	recipes = append(recipes, recipe)  // save to the local memory
-	c.JSON(http.StatusOK, gin.H{"id": recipe})
+	
+	c.JSON(http.StatusOK, gin.H{"id": recipe})  // return response to the client
+}
 
+func ListRecipesHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{"recipes": recipes})
 }
 
 func main() {
 	router := gin.Default()
 
+	router.GET("/recipes", ListRecipesHandler)
 	router.POST("/recipes", NewRecipeHandler)
 
 	router.Run()
--- building-distributed-applications-in-gin/chap_02/recipes-api/my_requests/recipes/post.py
+++ building-distributed-applications-in-gin/chap_02/recipes-api/my_requests/recipes/post.py
@@ -1,22 +1,38 @@
-from faker import Faker
-
 import requests
 import json
 
-fake = Faker()
+data = {
+    "id": "c0283p3d0cvuglq85log",
+    "name": "Oregano Marinated Chicken",
+    "tags": [
+            "main",
+            "chicken"
+    ],
+    "ingredients": [
+        "4 (6 to 7-ounce) boneless skinless chicken breasts\r",
+        "10 grinds black pepper\r",
+        "1/2 tsp salt\r",
+        "2 tablespoon extra-virgin olive oil\r",
+        "1 teaspoon dried oregano\r",
+        "1 lemon, juiced"
+    ],
+    "instructions": [
+        "To marinate the chicken: In a non-reactive dish, combine the lemon juice, olive oil, oregano, salt, and pepper and mix together",
+        " Add the chicken breasts to the dish and rub both sides in the mixture",
+        " Cover the dish with plastic wrap and let marinate in the refrigerator for at least 30 minutes and up to 4 hours",
+        "\r\n\r\nTo cook the chicken: Heat a nonstick skillet or grill pan over high heat",
+        " Add the chicken breasts and cook, turning once, until well browned, about 4 to 5 minutes on each side or until cooked through",
+        " Let the chicken rest on a cutting board for a few minutes before slicing it into thin strips"
+    ],
+    "publishedAt": "2021-01-17T19:28:52.803062+01:00"
+}
 
-name = fake.text()[:20]
-tags = fake.
-ingredients
-instructions
-publishedAt
 
+endpoint = f"http://127.0.0.1:8080/recipes/"
 
-url = f"http://127.0.0.1:8080/{name}/"
+post_request = requests.post(endpoint, json=data)
 
-get_request = requests.get(url)
-
-response = get_request.json()
+response = post_request.json()
 json_format = json.dumps(response, indent=4, sort_keys=True)
 
 print(json_format)
