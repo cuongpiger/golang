fac709d267e7a6ce4ce4cf48ba78a544e9157064
cuongpiger <cuongpigerr@gmail.com>
2022-10-16 17:05:47 +0700

    This is Manh Cuong's commit 🌻.

---
+++ chap08/ranging.go
@@ -0,0 +1,18 @@
+package main
+
+import "time"
+
+func main() {
+	ch := make(chan int)
+
+	go func() {
+		ch <- 1
+		time.Sleep(1 * time.Second)
+		ch <- 2
+		close(ch)
+	}()
+
+	for v := range ch {
+		println(v)
+	}
+}
