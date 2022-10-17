bef49d3cac7a5609a2ca0ef5c2d66d1cfbf8a325
cuong.duong2 <cuong.duong2@tiki.vn>
2022-10-17 11:31:41 +0700

    nothing to commit

--- chap09/barrier/main.go
+++ chap09/barrier/main.go
@@ -10,34 +10,55 @@ import (
 	"time"
 )
 
-var timeoutMilliseconds = 5000
+const timeoutMilliseconds = 5000
 
 type barrierResp struct {
 	Err  error
 	Resp string
 }
 
+func makeRequest(out chan<- barrierResp, url string) {
+	res := barrierResp{}
+	client := http.Client{
+		Timeout: time.Duration(time.Duration(timeoutMilliseconds) * time.Millisecond),
+	}
+
+	resp, err := client.Get(url)
+	if err != nil {
+		res.Err = err
+		out <- res
+		return
+	}
+
+	byt, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		res.Err = err
+		out <- res
+		return
+	}
+
+	res.Resp = string(byt)
+	out <- res
+}
+
 func barrier(endpoints ...string) {
-
 	requestNumber := len(endpoints)
-
 	in := make(chan barrierResp, requestNumber)
 	defer close(in)
 
 	responses := make([]barrierResp, requestNumber)
-
 	for _, endpoint := range endpoints {
 		go makeRequest(in, endpoint)
 	}
 
 	var hasError bool
 	for i := 0; i < requestNumber; i++ {
-		// Block the execution waiting for data from the in channel
 		resp := <-in
 		if resp.Err != nil {
-			fmt.Println("ERROR: ", resp.Err)
+			fmt.Println("Error: ", resp.Err)
 			hasError = true
 		}
+
 		responses[i] = resp
 	}
 
@@ -46,51 +67,13 @@ func barrier(endpoints ...string) {
 			fmt.Println(resp.Resp)
 		}
 	}
-
 }
 
-// makeRequest performs HTTP GET request to an url and accepts a channel to output barrierResp values
-func makeRequest(out chan<- barrierResp, url string) {
-	res := barrierResp{}
-
-	// Create the HTTP client and set the timeout
-	client := http.Client{
-		Timeout: time.Duration(time.Duration(timeoutMilliseconds) * time.Millisecond),
-	}
-
-	// Perform the HTTP GET request
-	resp, err := client.Get(url)
-	if err != nil {
-		res.Err = err
-		out <- res
-		return
-	}
-
-	// Parse the response to a []byte
-	byt, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		res.Err = err
-		out <- res
-		return
-	}
-
-	// Send it throught the channel
-	res.Resp = string(byt)
-	out <- res
-}
-
-// captureBarrierOutput will capture the outputs in stdout
 func captureBarrierOutput(endpoints ...string) string {
-	// Create a pipe that allows us to connect
-	// an io.Writer interface to an io.Reader interface
 	reader, writer, _ := os.Pipe()
-
 	os.Stdout = writer
-
 	out := make(chan string)
 
-	// Copies reader input to a bytes buffer before sending
-	// the contents of the buffer through a channel
 	go func() {
 		var buf bytes.Buffer
 		io.Copy(&buf, reader)
@@ -98,11 +81,22 @@ func captureBarrierOutput(endpoints ...string) string {
 	}()
 
 	barrier(endpoints...)
-
-	// Close the writer to signal the Goroutine that
-	// no more input is going to come to it
 	writer.Close()
 	temp := <-out
 
 	return temp
+}
+
+func main() {
+	endpoints := []string{"https://catfact.ninja/fact", "https://api.coindesk.com/v1/bpi/currentprice.json"}
+	result := captureBarrierOutput(endpoints...)
+	println(result)
+
+	endpoints2 := []string{"https://catfact.ninja/fact", "https://api.coindesk.com/v1/bpi/currentprices.json"} // the second endpoint is incorrect
+	result2 := captureBarrierOutput(endpoints2...)
+	println(result2)
+
+	endpoints3 := []string{"https://catfact.ninja/fact", "https://api.coindesk.com/v1/bpi/currentprices.json"} // the second endpoint is incorrect
+	result3 := captureBarrierOutput(endpoints3...)
+	println(result3)
 }
